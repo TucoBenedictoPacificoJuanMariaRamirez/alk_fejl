eed71a211c6fb2cb21fd93a3776b222c03006f01
Bence <truewerassad@gmail.com>
2018-10-11 20:31:45 +0200

    default spring application added

---
+++ src/main/java/hu/elte/alkfejl/etterem/EtteremApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.etterem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtteremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtteremApplication.class, args);
+	}
+}
