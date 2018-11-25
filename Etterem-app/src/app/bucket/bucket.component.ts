5a8d09b81386819e5b996e0baf62ac28b0075d76
Bence <truewerassad@gmail.com>
2018-11-25 16:34:25 +0100

    deleting from bucket does not work yet

--- Etterem-app/src/app/bucket/bucket.component.ts
+++ Etterem-app/src/app/bucket/bucket.component.ts
@@ -8,12 +8,30 @@ import { MenuService } from '../services/menu.service';
 })
 export class BucketComponent implements OnInit {
   private _menus: Menu[];
+  private selectedOptions: Menu[];
   constructor(
     private _menuService: MenuService
   ) { }
 
   ngOnInit() {
     this._menus = this._menuService.getMenus();
+    this.selectedOptions = [];
+  }
+
+  public removeItems(event: Event): void {
+    console.log('belemegy');
+    const currentBucket = this;
+    this.selectedOptions.forEach(function(value) {
+      const index = currentBucket.selectedOptions.indexOf(value, 0);
+      if (index > -1) {
+        currentBucket.selectedOptions.splice(index, 1);
+      }
+      console.log(value + 'deleted');
+    });
+  }
+
+  public updateSelected(event: Event, menu: Menu): void {
+    this.selectedOptions.push(menu);
   }
 
 }
